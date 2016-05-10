52b31a574cdc5633133f8c74f51234d4d52338fb
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2016-05-09 21:38:18 -0700

    readtable

--- age_demean.m
+++ age_demean.m
@@ -1,3 +1,3 @@
-M = textscan(fopen('participants.tsv'), '%s %f %s %f %*[^\n]', 'Delimiter', '\t', 'headerlines', 1);
+M = readtable('participants.tsv', 'FileType', 'text', 'Delimiter', '\t');
 
 age = M{1,4};
